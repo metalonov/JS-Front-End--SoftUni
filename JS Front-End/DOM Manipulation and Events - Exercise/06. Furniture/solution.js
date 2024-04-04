6c6aa573b7359d09120c34f1f24968861055b9ba
metalonov <118399788+metalonov@users.noreply.github.com>
2024-04-04 14:59:06 +0300

    etc.

--- JS Front-End/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,90 @@
 function solve() {
+  const firstTextAreaElement = document.querySelector('#exercise textarea:first-of-type');
+  const secondTextAreaElement = document.querySelector('#exercise textarea:last-of-type');
+  const generateButtonElement = document.querySelector('#exercise button:first-of-type');
+  const buyButtonElement = document.querySelector('#exercise button:last-of-type');
+  const furnitureTbodyElement = document.querySelector('.table tbody');
 
-  //TODO...
+  
+  generateButtonElement.addEventListener('click', () => {
+    const furnitures = JSON.parse(firstTextAreaElement.value);
+    
+    for (const furniture of furnitures) {
+      const imgElement = document.createElement('img');
+      imgElement.src = furniture.img;
+
+      const imageTdElement = document.createElement('td');
+      imageTdElement.appendChild(imgElement);
+
+      const pNameElement = document.createElement('p');
+      pNameElement.textContent = furniture.name;
+      const tdNameElement = document.createElement('td');
+      tdNameElement.appendChild(pNameElement);
+
+      const pricePElement = document.createElement('p');
+      pricePElement.textContent = furniture.price;
+      const priceTdElement = document.createElement('td');
+      priceTdElement.appendChild(pricePElement);
+
+      const decPElement = document.createElement('p');
+      decPElement.textContent = furniture.decFactor;
+      const decTdElement = document.createElement('td');
+      decTdElement.appendChild(decPElement);
+
+      const markElement = document.createElement('input');
+      markElement.setAttribute('type', 'checkbox');
+      const markTdElement = document.createElement('td');
+      markTdElement.appendChild(markElement);
+
+      const furnitureTrElement = document.createElement('tr');
+      furnitureTrElement.appendChild(imageTdElement);
+      furnitureTrElement.appendChild(tdNameElement);
+      furnitureTrElement.appendChild(priceTdElement);
+      furnitureTrElement.appendChild(decTdElement);
+      furnitureTrElement.appendChild(markTdElement);
+      
+      furnitureTbodyElement.appendChild(furnitureTrElement);
+    }
+  })
+    
+  
+  buyButtonElement.addEventListener('click', () => {
+    let names = [];
+    let totalPrice = 0;
+    let avgDecFactor = [];
+
+    Array.from(furnitureTbodyElement.children)
+    .forEach(furnitureElement => {
+      const markedForBuying = furnitureElement.querySelector('input[type=checkbox]');
+      if (!markedForBuying.checked) {
+        return;
+      }
+      const name = furnitureElement.children.item(1).textContent;
+      const price = Number(furnitureElement.children.item(2).textContent);
+      const decorationFactor = Number(furnitureElement.children.item(3).textContent);
+      names.push(name);
+      totalPrice += price;
+      avgDecFactor.push(decorationFactor);
+    });
+    
+    secondTextAreaElement.textContent += `Bought furniture: ${names.join(', ')}\n`;
+    secondTextAreaElement.textContent += `Total price: ${totalPrice.toFixed(2)}\n`;
+    secondTextAreaElement.textContent += `Average decoration factor: ${average(avgDecFactor)}`
+  })
+
+
+  function average(arr) {
+    let sum = 0;
+    for (const number of arr) {
+      sum += number;
+    }
+
+    return sum / arr.length;
+  }
 }
+
+
+
+// [{"name": "Sofa", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 150, "decFactor": 1.2}]
+// [{"name": "Table", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 180, "decFactor": 0.2}]
+// [{"name": "Desk", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 210, "decFactor": 0.7}]
--- JS Front-End/DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,60 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const fromUnitOptionElement = document.getElementById('inputUnits');
+    const fromUnitOptionValueElement = document.getElementById('inputDistance');
+    const convertButtonElement = document.getElementById('convert');
+
+    const toUnitOptionElement = document.getElementById('outputUnits');
+    const toDistanceOptionValueElement = document.getElementById('outputDistance');
+
+
+    convertButtonElement.addEventListener('click', () => {
+        const milimeters = convertToMilimeters(Number(fromUnitOptionValueElement.value), Number(fromUnitOptionElement.selectedIndex));
+        console.log(milimeters);
+        const resultingConvertedDistance = convertTo(milimeters, Number(toUnitOptionElement.selectedIndex));
+        toDistanceOptionValueElement.value = resultingConvertedDistance.toFixed(4);
+    })
+    
+
+    function convertTo(value, unit) {
+        switch (unit) {
+            case unit = 0:
+                return value / 1000000;
+            case unit = 1:
+                return value / 1000;
+            case unit = 2:
+                return value / 10;
+            case unit = 3:
+                return value;
+            case unit = 4:
+                return value / 1609000;
+            case unit = 5:
+                return value / 914.4;
+            case unit = 6:
+                return value / 304.8;
+            case unit = 7:
+                return value / 25.4;
+        }
+    }
+
+    function convertToMilimeters(value, unit) {
+
+        switch (unit) {
+            case unit = 0:
+                return value * 1000000;
+            case unit = 1:
+                return value * 1000;
+            case unit = 2:
+                return value * 10;
+            case unit = 3:
+                return value;
+            case unit = 4:
+                return value * 1609000;
+            case unit = 5:
+                return value * 914.4;
+            case unit = 6:
+                return value * 304.8;
+            case unit = 7:
+                return value * 25.4;
+        }
+    }
 }
